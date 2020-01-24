8eff3fe3345cc687ad0b08fedd5b747eed2d25b1
Thomas Guyader <thomas.guyader@gouv.nc>
2020-01-24 14:42:03 +1100

    fix(base): install fonctionnelle

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import eel
 
-eel.init('web') # or the name of your directory
-eel.start('index.html', size=(600, 400))
+eel.init('web')
+eel.start('index.html', size=(600, 400))@eel.expose
+def hello_world():
+    return "Hello from python"
---
+++ web/precache-manifest.6147e5b834665fd72785b5803b16413b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612c1658e506fd45ddcf",
+    "url": "/css/app.688045ec.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bde58fe30ba4e018e09b1657d378a4db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe517813b643e5ca69c9",
+    "url": "/js/about.7d9379eb.js"
+  },
+  {
+    "revision": "612c1658e506fd45ddcf",
+    "url": "/js/app.b6300b5a.js"
+  },
+  {
+    "revision": "6f93fb2e5f18372932d9",
+    "url": "/js/chunk-vendors.b42a6c66.js"
+  },
+  {
+    "revision": "b942b517f9bd486e8cc7edf3a86090ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
